4ee6ca0afea36eed2c1e7a462b47480512dfdb44
Jonas Fenchel <locken.kop@web.de>
2023-11-12 11:24:22 +0100

    add driverloading to webcam

--- peripherals/webcam.sh
+++ peripherals/webcam.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sudo modprobe v4l2loopback devices=1 video_nr=10 card_label="Canon 700D" exclusive_caps=1
 gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video10
+
