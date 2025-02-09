6fc493fbe146e3aef0f0d29ffa778ec37140a0b8
Jonas Fenchel <lockenkop@proton.me>
2025-02-09 02:48:57 +0100

    update display scripts

--- scripts/display/kde/fernseher.sh
+++ scripts/display/kde/fernseher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-kscreen-doctor output.DP-2.disable output.DP-0.enable output.DP-0.mode.3840x2160@60 output.DP-0.priority.1
+kscreen-doctor output.DP-1.disable output.DP-2.enable output.DP-2.mode.3840x2160@60 output.DP-2.priority.1
--- scripts/display/kde/standard.sh
+++ scripts/display/kde/standard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-kscreen-doctor output.DP-2.enable output.DP-2.mode.5120x1440@120 output.DP-2.priority.1 output.DP-2.geo output.DP-0.enable output.DP-0.mode.3840x2160@60 output.DP-0.priority.2
+kscreen-doctor output.DP-1.enable output.DP-1.mode.5120x1440@120 output.DP-1.priority.1 output.DP-2.enable output.DP-2.mode.3840x2160@60 output.DP-2.priority.2
